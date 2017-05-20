c4b3e75d4933130b0358937659ff87be00e087c3
Анна Колосова <anyakolosova1@gmail.com>
2017-05-20 20:01:39 +0300

    12

--- WindowsFormsApplication5/Program.cs
+++ WindowsFormsApplication5/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApplication5
         [STAThread]
         static void Main()
         {
+            int five = 5;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
